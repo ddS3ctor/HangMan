3227cbea49f2dcd6192dd751bc84ac43dec5b2c8
maphane123a <maphane123a@student.wethinkcode.co.za>
2024-08-28 07:23:53 +0200

    Added column-id and score row

--- hangman_cli.py
+++ hangman_cli.py
@@ -1,5 +1,5 @@
+import random
 import requests
-
 
 url = 'http://127.0.0.1:5000'
 response = requests.get(url + "/words")
@@ -10,16 +10,33 @@ def play():
     #     continue
     print_table(words[2])
 
+def solve(word):
+    fill_chars = []
+
+    print_table(word)
+
 
 def print_table(word):
     colums = len(word)
+    score = 0
 
     print('+---' * colums + '+')
     print(end='|')
-    for colum in range(colums):
-        print(end=' ' + word[colum] + ' |')
+
+    for column in range(colums): print(end=' ' + str(column+1) + ' |')
+    print()
+
+    print('+---' * colums + '+')
+    print(end='|')
+    for column in range(colums): print(end=' ' + word[column] + ' |')
     print()
     print('+---' * colums + '+')
+
+    if len(str(score)) == 1: print(end='| score:  ' + str(score) + ' |')
+    elif len(str(score)) == 2: print(end='| score: ' + str(score) + ' |')
+    elif len(str(score)) == 3: print(end='| score: ' + str(score) + '|')
+    print()
+    print('+' + '---' * 3 + '--+')
 
 
 if __name__ == '__main__':
